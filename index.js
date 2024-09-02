afb9d392b81f2ad8b21b406a278e2f2d34731856
notoiro297 <notoiro297@gmail.com>
2024-09-03 08:10:32 +0900

    package

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const sample = process.argv[2].split(':');
+const copy = (obj) => JSON.parse(JSON.stringify(obj));
 
 const notes = {
   'c':  0,
@@ -15,145 +15,167 @@ const notes = {
   'b': 11
 }
 
-function main(){
-  const lyrics = Array.from(sample[0]);
-  const frames = sample[1].replace(/,/, "");
+const blank_note = { key: null, lyric: '', octave: null, length: 1 };
 
-  const song = parse(frames, lyrics);
-
-  for(let s of song.track){
-    s.frame = calc_frame(calc_ms(s.length, song.tempo));
-    s.key = note_to_midi(s.key, s.octave);
+class VML{
+  constructor(frame_rate = 93.75){
+    this.frame_rate = frame_rate;
   }
 
-  console.log(song);
-}
+  parse(text){
+    const sample = text.split(':');
 
-function parse(frames, lyrics){
-  const frame_arr = Array.from(frames);
+    const lyrics = Array.from(sample[0]);
+    const frames = sample[1].replace(/,/, "");
 
-  const results = [];
+    return this._parse(frames, lyrics);
+  }
 
-  // C4, o4
-  let octave = 4;
-  let is_wait_octave = false;
-  let is_wait_tempo= false;
-  let current = null;
-  let tempo = "120";
+  parse_voicevox(text, key_range_fix = 0){
+    const song = this.parse(text);
 
-  for(let f of frame_arr){
-    switch(true){
-      // cdefgabが音程、rが休符、oがオクターブ、tがテンポ
-      case /[cdefgabrot]/i.test(f):
-        is_wait_octave = false;
-        is_wait_tempo = false;
-        if(f === 'o'){
-          is_wait_octave = true;
-          continue;
-        }
+    for(let s of song.track){
+      s.length = this.calc_frame(this.calc_ms(s.length, song.tempo));
+      s.key = this.note_to_midi(s.key, s.octave, key_range_fix);
+    }
 
-        if(f === 't'){
-          is_wait_tempo = true;
-          tempo = "";
-          continue;
-        }
+    song.track.unshift(blank_note);
 
-        results.push(copy(current));
-        current = null;
+    return song;
+  }
 
-        if(f === 'r'){
-          current = {
-            key: null,
-            lyric: "",
-            octave: null,
-            length: ""
-          };
-        }else{
-          current = {
-            key: f.toLowerCase(),
-            lyrics: lyrics.shift(),
-            octave: octave,
-            length: ""
+  _parse(frames, lyrics){
+    const frame_arr = Array.from(frames);
+
+    const results = [];
+
+    // C4, o4
+    let octave = 4;
+    let is_wait_octave = false;
+    let is_wait_tempo= false;
+    let current = null;
+    let tempo = "120";
+
+    for(let _f of frame_arr){
+      let f = _f.toLowerCase();
+      switch(true){
+        // cdefgabが音程、rが休符、oがオクターブ、tがテンポ
+        case /[cdefgabrot]/.test(f):
+          is_wait_octave = false;
+          is_wait_tempo = false;
+          if(f === 'o'){
+            is_wait_octave = true;
+            continue;
           }
-        }
 
-        break;
-      case f === '<':
-        octave--;
-        break;
-      case f === '>':
-        octave++;
-        break;
-      case f === '.':
-        current.length += '.';
-        break;
-      case f === '^':
-        current.length += '^';
-        break;
-      case /[0-9]/.test(f):
-        if(is_wait_octave){
-          octave = parseInt(f);
-          continue;
-        }
+          if(f === 't'){
+            is_wait_tempo = true;
+            tempo = "";
+            continue;
+          }
 
-        if(is_wait_tempo){
-          tempo += f;
-          continue;
-        }
+          results.push(copy(current));
+          current = null;
 
-        current.length += f;
-        break;
-      case /[\+#]/.test(f):
-        current.key += "+";
-        break;
-      case f === '-':
-        current.key += "-";
-        break;
-    }
-  }
+          if(f === 'r'){
+            current = {
+              key: null,
+              lyric: "",
+              octave: null,
+              length: ""
+            };
+          }else{
+            current = {
+              key: f,
+              lyric: lyrics.shift(),
+              octave: octave,
+              length: ""
+            }
+          }
 
-  results.push(copy(current));
-  results.shift();
+          break;
+        case f === '<':
+          octave--;
+          break;
+        case f === '>':
+          octave++;
+          break;
+        case f === '.':
+          current.length += '.';
+          break;
+        case f === '^':
+          current.length += '^';
+          break;
+        case /[0-9]/.test(f):
+          if(is_wait_octave){
+            octave = parseInt(f);
+            continue;
+          }
 
-  return {
-    tempo: parseInt(tempo),
-    track: results
-  };
-}
+          if(is_wait_tempo){
+            tempo += f;
+            continue;
+          }
 
-function note_to_midi(note, octave){
-  return note === null ? null :  notes[note] + (octave + 1) * 12;
-}
-
-function calc_ms(score_length, bpm = 120){
-  let result = 0;
-
-  for(let s of score_length.split('^')){
-    let length;
-    let dot = false;
-
-    if(s.includes('.')){
-      length = parseInt(s.replace('.', ''), 10);
-      dot = true;
-    }else{
-      length = parseInt(s, 10);
+          current.length += f;
+          break;
+        case /[\+#]/.test(f):
+          current.key += "+";
+          break;
+        case f === '-':
+          current.key += "-";
+          break;
+      }
     }
 
-    let ms = 240/bpm/length*1000;
+    results.push(copy(current));
+    results.shift();
 
-    if(dot) ms = ms * 1.5;
-
-    result += ms;
+    return {
+      tempo: parseInt(tempo),
+      track: results
+    };
   }
-  return result;
+
+  note_to_midi(note, octave, key_range_fix = 0){
+    return note === null ? null :  (notes[note] + (octave + 1) * 12) + key_range_fix;
+  }
+
+  calc_ms(score_length, bpm = 120){
+    let result = 0;
+
+    for(let s of score_length.split('^')){
+      let length;
+      let dot = false;
+
+      if(s.includes('.')){
+        length = parseInt(s.replace('.', ''), 10);
+        dot = true;
+      }else{
+        length = parseInt(s, 10);
+      }
+
+      let ms = 240/bpm/length*1000;
+
+      if(dot) ms = ms * 1.5;
+
+      result += ms;
+    }
+    return result;
+  }
+
+  calc_frame(ms){
+    return Math.round(ms * this.frame_rate /1000);
+  }
 }
 
-function calc_frame(ms, frame_rate = 93.75){
-  return Math.round(ms * frame_rate /1000);
-}
+module.exports = VML;
 
-function copy(obj){
-  return JSON.parse(JSON.stringify(obj));
-}
+//function main(){
+//  const vml = new VML();
+//
+//  console.log(vml.parse_voicevox(process.argv[2], +7));
+//}
+//
+//main();
 
-main();
